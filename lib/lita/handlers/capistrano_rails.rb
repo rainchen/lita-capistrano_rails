cb0a1e0a3e1081dc607d26af82298ae91516dc09
RainChen <hirainchen@gmail.com>
2016-01-02 19:01:50 +0800

    lita-capistrano_rails: add "deploy list" command to list available apps for deploy

--- lib/lita/handlers/capistrano_rails.rb
+++ lib/lita/handlers/capistrano_rails.rb
@@ -1,7 +1,43 @@
 module Lita
   module Handlers
     class CapistranoRails < Handler
-      # insert handler code here
+      # example:
+      # config.handlers.capistrano_rails.apps = {
+      #   'app1' => {
+      #     git: 'git@git.example.com:account/app1.git',
+      #     rails_env: 'production',
+      #   },
+      #   'app2' => {
+      #     git: 'git@git.example.com:account/app2.git',
+      #     rails_env: 'staging',
+      #   },
+      # }
+      config :apps, type: Hash, required: true
+
+      on :loaded, :define_routes
+
+      def define_routes(payload)
+        define_static_routes
+      end
+
+      def deploy_list_apps(response)
+        response.reply_privately('Available apps:')
+        apps = config.apps.map do |app, app_config|
+          "#{app}[#{app_config[:rails_env]}]"
+        end
+        response.reply_privately(apps )
+      end
+
+      private
+
+      def define_static_routes
+        self.class.route(
+          %r{^deploy\s+list},
+          :deploy_list_apps,
+          command: true,
+          help: { "deploy list" => "List available apps for deploy"}
+        )
+      end
 
       Lita.register_handler(self)
     end
